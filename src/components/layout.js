51c464c68e899d6af8dc4d9eb9b227d6b49cc378
Curtis Cummings <curtis.cummings@pixeldashstudios.com>
2023-09-27 16:25:27 -0500

    fix layout.js

---
+++ src/components/layout.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+import nogdImage from '../images/nogd.png';
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <div>
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand href="/">
+            <img
+              src={nogdImage}
+              fluid
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+              NOGD
+              
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/contact">Contact</Nav.Link>
+                <Nav.Link href="/about">About Us</Nav.Link>
+                <Nav.Link href="/donate">Donate</Nav.Link>
+                <NavDropdown title="Events" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="/show-n-tell">Show N Tell</NavDropdown.Item>
+                  <NavDropdown.Item href="/meetup">
+                    Third Thursday Series
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/game-jams">
+                    Game Jams
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+      <main>{children}</main>
+      <footer className="footer">
+      {/* footer stuff goes here */}
+      </footer>
+    </>
+  );
+}
