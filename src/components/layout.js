256723feb5c7f35780e57da68ae7d97c64dc1d9f
Curtis Cummings <curtis.cummings@pixeldashstudios.com>
2023-09-27 20:01:39 -0500

    update index page

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,51 +6,48 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Image from "react-bootstrap/Image";
+import Card from "react-bootstrap/Card";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./layout.css";
 
 export default function Layout({ children }) {
-    return (
-        <>
-            <Container fluid as={'header'}>
-                <Navbar expand="lg" className="bg-body-tertiary">
-                    <Container as={'nav'}>
-                        <Navbar.Brand href="/">
-                            <img
-                                src={nogdImage}
-                                height={75}
-                                fluid
-                            />
-                        </Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                <Nav.Link href="/">Home</Nav.Link>
-                                <Nav.Link href="/contact">Contact</Nav.Link>
-                                <Nav.Link href="/about">About Us</Nav.Link>
-                                <Nav.Link href="/donate">Donate</Nav.Link>
-                                <NavDropdown title="Events" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="/show-n-tell">
-                                    Show N Tell
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="/meetup">
-                                    Third Thursday Series
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="/game-jams">
-                                    Game Jams
-                                </NavDropdown.Item>
-                                </NavDropdown>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            </Container>
-            <Container as={'main'}>
-                {children}
-            </Container>
-            <Container fluid as={'footer'}>
-                Footer
-            </Container>
-        </>
-    );
+  return (
+    <Container
+      style={{ paddingLeft: 0, paddingRight: 0, marginLeft: 0, marginRight: 0 }}
+      fluid
+      as="body"
+    >
+      <Container fluid as={"header"}>
+        <Navbar sticky="top" expand="lg">
+          <Navbar.Brand className="justify-content-start" href="/">
+            <Image src={nogdImage} height={75} fluid />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+              <Nav.Link href="/about">About Us</Nav.Link>
+              <Nav.Link href="/donate">Donate</Nav.Link>
+              <NavDropdown title="Events" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/show-n-tell">
+                  Show N Tell
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/meetup">
+                  Third Thursday Series
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/game-jams">Game Jams</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+      <Container as={"main"}>{children}</Container>
+      <Container fluid as={"footer"}>
+        Footer
+      </Container>
+    </Container>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,31 +4,61 @@
 //Last edited:9/20/2023
 
 //classic imports
-import React from 'react';
-import { Link } from 'gatsby';
-import nogdImage from '../images/nogd.png';
-import './index.css';
+import React from "react";
+import { Link } from "gatsby";
+
+import { Card, Container, Image, Row, Col } from "react-bootstrap";
+
+import nogdImage from "../images/nogd.png";
+import "./index.css";
 
 function IndexPage() {
   return (
-      <>
-        <div className="header">
-          <h1>New Orleans Game Developer</h1>
-          <p>Still unfinished, working on it!</p>
-          <ul>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
-        </div>
-        <div>
-          <iframe
-            src="https://calendar.google.com/calendar/embed?src=c_817psp5emlgh8vgg432udrtv20%40group.calendar.google.com&ctz=America%2FChicago"
-            style={{ border: 0, width: '100%', height: '600px' }}
-            frameBorder="0"
-            scrolling="no"
-          ></iframe>
-        </div>
-      </>
+    <>
+      <Row>
+      <Container>
+        <h1>New Orleans Game Developers</h1>
+      </Container>
+      </Row>
+      <Row>
+          <Col lg={4}>
+            <Card>
+              <Card.Body>
+                <Card.Title>Who?</Card.Title>
+                <Card.Text>We are a bunch of game developers in the Greater New Orleans area who meet on a monthly basis to network, collaborate, and share knowledge.</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col  lg={4}>
+            <Card>
+              <Card.Body>
+                <Card.Title>What?</Card.Title>
+                <Card.Text>Each month we have a rotation selection of events that help the gamedev community present on interesting topics, showcase their projects, and encourage game development in the local area.</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={4}>
+            <Card>
+              <Card.Body>
+                <Card.Title>When & Where?</Card.Title>
+                <Card.Text>Each month we have a rotation selection of events that help the gamedev community present on interesting topics, showcase their projects, and encourage game development in the local area.</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+
+      </Row>
+      <Row>
+        <p>Hang out with us</p>
+      </Row>
+      <Row>
+        <iframe
+          src="https://calendar.google.com/calendar/embed?src=c_817psp5emlgh8vgg432udrtv20%40group.calendar.google.com&ctz=America%2FChicago"
+          style={{ border: 0, width: "100%", height: "600px" }}
+          frameBorder="0"
+          scrolling="no"
+        ></iframe>
+      </Row>
+    </>
   );
 }
 
